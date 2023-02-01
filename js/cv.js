b563e7acc83ee0011ed4946579a8f9f4634a881b
miltonriver <miltonriver66@gmail.com>
2023-02-01 16:39:27 -0300

    primer commit de mi CV para proyecto final

---
+++ js/cv.js
@@ -0,0 +1,8 @@
+document.getElementById('boton_cambio').onclick = function () {
+    document.getElementById('mi_encabezado').style.backgroundColor='red';
+}
+
+document.getElementById("boton_reinicio").onclick = function(){
+    document.getElementById("mi_encabezado").style.backgroundColor= "blue";
+}
+
